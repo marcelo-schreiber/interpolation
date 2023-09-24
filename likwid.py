bdad71e25a99299cf92dfd6ae4003cd053639ab4
marcelo-notebook <marcelorissette15@gmail.com>
2023-09-24 17:29:58 -0300

    add skip lines

--- likwid.py
+++ likwid.py
@@ -7,5 +7,9 @@ xe = float(input("Digite o valor de x: "))
 # xe = float(input(""))
 
 system(f"likwid-perfctr -C 3 -g FLOPS_DP -m ./interpola {xe} < pontos.in > resultado.out")
-system("grep \"MFLOP/s\" resultado.out")
+
+print("") # Pula linha
+system("grep -e \"MFLOP/s\" -e \"newton\" -e \"lagrange\" resultado.out")
+print("") # Pula linha
+
 system("make purge")
