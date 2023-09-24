195c7158ebc7dbf7119ac960a4857b1b0f7e001f
marcelo-notebook <marcelorissette15@gmail.com>
2023-09-24 17:01:46 -0300

    fix grep on output file

--- likwid.py
+++ likwid.py
@@ -3,9 +3,9 @@ from os import system
 
 system("make")
 
-# xe = float(input("Digite o valor de x: "))
-xe = float(input(""))
+xe = float(input("Digite o valor de x: "))
+# xe = float(input(""))
 
 system(f"likwid-perfctr -C 3 -g FLOPS_DP -m ./interpola {xe} < pontos.in > resultado.out")
-system("grep \"DP MFLOP/S\" < resultado.out")
+system("grep \"MFLOP/s\" resultado.out")
 system("make purge")
